ab3174681edfe6f3238abc93859b5078bb5e89aa
SouraVNandA <souravkumarnanda4@gmail.com>
2019-01-17 01:53:45 +0530

    Some random changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -40,7 +40,7 @@
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 	
-	$('#mu-event-counter').countdown('2018/02/20').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2019/03/15').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
